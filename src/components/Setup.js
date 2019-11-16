50e379e0d5d819abddee65d1bc212dfcb95cda30
Tobias Fried <friedtm@gmail.com>
2019-11-16 01:28:42 -0500

    Reformat help menus

--- src/components/Setup.js
+++ src/components/Setup.js
@@ -5,6 +5,7 @@ import { initializeApp, resetApp, createUser } from "../actions";
 
 const Setup = ({ init, createUser }) => {
   const [deviceAddress, setDeviceAddress] = useState("");
+  const [expanded, setExpanded] = useState(null);
 
   useEffect(() => {
     discoverDevices();
@@ -21,6 +22,14 @@ const Setup = ({ init, createUser }) => {
 
   const handleSubmit = () => {
     createUser(deviceAddress);
+  };
+
+  const expand = index => {
+    if (index == expanded) {
+      setExpanded(null);
+    } else {
+      setExpanded(index);
+    }
   };
 
   const renderContent = () => {};
@@ -47,81 +56,102 @@ const Setup = ({ init, createUser }) => {
       <div className="ui secondary message">
         <div className="header">Device not conneted</div>
       </div>
-    )
+    );
   };
 
   const renderHelp = () => {
     return (
       <div className="ui fluid styled accordion">
-        <div className="title">
+        <div
+          className={`${expanded == 0 ? "active" : null} title`}
+          onClick={() => expand(0)}
+        >
           <i className="dropdown icon"></i>
           Finding your Device IP
         </div>
-        <div className="content">
-          <div className="transition hidden">
-            <div>
-              If your device is not automatically discovered, you must manually
-              enter the device IP address above.
-            </div>
-            <div>
-              Your IP address can be found by using one of the following
-              methods:
-            </div>
+        <div className={`${expanded == 0 ? "active" : null} content`}>
+          <div>
+            If your device is not automatically discovered, you must manually
+            enter the device IP address above.
+          </div>
+          <div>
+            Your IP address can be found by using one of the following methods:
+          </div>
+          <div className="ui divider" />
+          <div>
+            Use the{" "}
+            <a href="https://discovery.meethue.com" target="_blank">
+              Hue Discovery Tool
+            </a>
             <div className="ui ordered list">
               <div className="item">
-                Use the{" "}
-                <a href="https://discovery.meethue.com">Hue Discovery Tool</a>
-                <div className="list">
-                  <div className="item">
-                    In a browser, visit the Discovery Tool, making sure you are
-                    on the same network as your device
-                  </div>
-                  <div className="item">
-                    If a device is found, the IP address will appear next to{" "}
-                    <b>"internalipaddress":</b>
-                  </div>
-                </div>
+                In a browser, visit the Discovery Tool, making sure you are on
+                the same network as your device
               </div>
               <div className="item">
-                Log into your wireless router and look Philips Hue up in the
-                DHCP table
-              </div>
-              <div className="item">
-                Use the official <a href="">Hue App</a>
-                <div className="list">
-                  <div className="item">
-                    Download the official Philips Hue app
-                  </div>
-                  <div className="item">
-                    Connect your phone to the network the hue bridge is on
-                  </div>
-                  <div className="item">Start the hue app</div>
-                  <div className="item">Push link connect to the bridge</div>
-                  <div className="item">
-                    Go to <b>Settings > My Bridge > Network</b> and switch off
-                    the DHCP toggle
-                  </div>
-                  <div className="item">
-                    Note the IP address, then switch DHCP back on
-                  </div>
-                </div>
-              </div>
-              <div className="item">
-                Use a UPnP discovery app to find Philips Hue in your network
+                If a device is found, the IP address will appear next to{" "}
+                <code>"internalipaddress":</code>
               </div>
             </div>
           </div>
+          <div className="ui divider" />
+          <div>
+            Log into your wireless router and look Philips Hue up in the DHCP
+            table
+          </div>
+          <div className="ui divider" />
+          <div>
+            Use the official{" "}
+            <a
+              href="https://www2.meethue.com/en-us/app/bridge#download-hue-app"
+              target="_blank"
+            >
+              Hue App
+            </a>
+            <div className="ui ordered list">
+              <div className="item">Download the official Philips Hue app</div>
+              <div className="item">
+                Connect your phone to the network the hue bridge is on
+              </div>
+              <div className="item">Start the hue app</div>
+              <div className="item">Push link connect to the bridge</div>
+              <div className="item">
+                Go to <b> Settings > My Bridge > Network </b> and switch off the
+                DHCP toggle
+              </div>
+              <div className="item">
+                Note the IP address, then switch DHCP back on
+              </div>
+            </div>
+          </div>
+          <div className="ui divider" />
+          <div>
+            Use a UPnP discovery app to find Philips Hue in your network
+          </div>
         </div>
-        <div className="title">
+        <div
+          className={`${expanded == 1 ? "active" : null} title`}
+          onClick={() => expand(1)}
+        >
           <i className="dropdown icon"></i>
           Connecting to your Philips Hue Bridge
         </div>
-        <div className="content">
-          <p>
-            There are many breeds of dogs. Each breed varies in size and
-            temperament. Owners often select a breed of dog that they find to be
-            compatible with their own lifestyle and desires from a companion.
-          </p>
+        <div className={`${expanded == 1 ? "active" : null} content`}>
+          Your device should be automatically detected if it is on the same
+          network as your computer. If you are on the same network and still do
+          not see a Device IP, try obtaining it using one of the methods above.
+          <div className="ui divider" />
+          <div className="ui ordered list">
+            <div className="item">
+              Enter your Device IP into the field above
+            </div>
+            <div className="item">
+              Press the <b> Pair </b> button on your Hue Bridge device
+            </div>
+            <div className="item">
+              Press the <b> Connect </b> button
+            </div>
+          </div>
         </div>
       </div>
     );
@@ -153,57 +183,6 @@ const Setup = ({ init, createUser }) => {
         </form>
       </div>
       {renderHelp()}
-      <div className="ui segment">
-        <h3 className="header">Finding your Device IP</h3>
-        <div>
-          If your device is not automatically discovered, you must manually
-          enter the device IP address above.
-        </div>
-        <div>
-          Your IP address can be found by using one of the following methods:
-        </div>
-        <div className="ui ordered list">
-          <div className="item">
-            Use the{" "}
-            <a href="https://discovery.meethue.com">Hue Discovery Tool</a>
-            <div className="list">
-              <div className="item">
-                In a browser, visit the Discovery Tool, making sure you are on
-                the same network as your device
-              </div>
-              <div className="item">
-                If a device is found, the IP address will appear next to{" "}
-                <b>"internalipaddress":</b>
-              </div>
-            </div>
-          </div>
-          <div className="item">
-            Log into your wireless router and look Philips Hue up in the DHCP
-            table
-          </div>
-          <div className="item">
-            Use the official <a href="">Hue App</a>
-            <div className="list">
-              <div className="item">Download the official Philips Hue app</div>
-              <div className="item">
-                Connect your phone to the network the hue bridge is on
-              </div>
-              <div className="item">Start the hue app</div>
-              <div className="item">Push link connect to the bridge</div>
-              <div className="item">
-                Go to <b>Settings > My Bridge > Network</b> and switch off the
-                DHCP toggle
-              </div>
-              <div className="item">
-                Note the IP address, then switch DHCP back on
-              </div>
-            </div>
-          </div>
-          <div className="item">
-            Use a UPnP discovery app to find Philips Hue in your network
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
@@ -212,7 +191,4 @@ const mapStateToProps = state => {
   return { init: state.init };
 };
 
-export default connect(
-  mapStateToProps,
-  { createUser, resetApp }
-)(Setup);
+export default connect(mapStateToProps, { createUser, resetApp })(Setup);
