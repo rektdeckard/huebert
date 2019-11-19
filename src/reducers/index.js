f5ffb74043588bd29d1f37080b5d2fbd005f6287
Tobias Fried <friedtm@gmail.com>
2019-11-19 00:00:44 -0500

    Added reducer templates for additional APIs

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,20 @@ import thunk from 'redux-thunk';
 
 import roomsReducer from './roomsReducer';
 import lightsReducer from './lightsReducer';
+import scenesReducer from './scenesReducer';
+import schedulesReducer from './schedulesReducer';
+import rulesReducer from './rulesReducer';
+import sensorsReducer from './sensorsReducer';
 import activeReducer from './activeReducer';
 import initReducer from './initReducer';
 
 const rootReducer = combineReducers({
-  rooms: roomsReducer,
   lights: lightsReducer,
+  rooms: roomsReducer,
+  scenes: scenesReducer,
+  schedules: schedulesReducer,
+  rules: rulesReducer,
+  sensors: sensorsReducer,
   active: activeReducer,
   init: initReducer
 });
