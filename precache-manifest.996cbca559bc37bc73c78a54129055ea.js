41a7355b3e5de5eb3bc31774dfcc41d459479a47
Tobias Fried <friedtm@gmail.com>
2019-12-05 01:27:17 -0500

    Updates

---
+++ precache-manifest.996cbca559bc37bc73c78a54129055ea.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce347eddd055345fa7b986df76a850f2",
+    "url": "/huebert/index.html"
+  },
+  {
+    "revision": "dc31684e735b66eba16e",
+    "url": "/huebert/static/js/2.138f0c86.chunk.js"
+  },
+  {
+    "revision": "7c5d75e33b1d13fb17bc",
+    "url": "/huebert/static/js/main.1d137d7c.chunk.js"
+  },
+  {
+    "revision": "ab07b1bc2531b7d65cc1",
+    "url": "/huebert/static/js/runtime-main.54a4f8d3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/huebert/precache-manifest.9a5add15bdd4530e0664379a3a78a0d9.js"
+  "/huebert/precache-manifest.996cbca559bc37bc73c78a54129055ea.js"
 );
 
 self.addEventListener('message', (event) => {
