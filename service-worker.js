004b42d8e9af51d0dc2a08089ef50e76753b884b
Tobias Fried <friedtm@gmail.com>
2019-12-10 02:18:49 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/huebert/precache-manifest.6516ab73828af5317a42aa0169218bcb.js"
+  "/huebert/precache-manifest.e11968493676379b4bb638375831922b.js"
 );
 
 self.addEventListener('message', (event) => {
