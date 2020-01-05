cee107b23a2cc50acc5c833226837761936a7d37
Tobias Fried <friedtm@gmail.com>
2020-01-05 15:54:09 -0500

    Add useThrottle hook to generify throttling of function calls

---
+++ src/hooks/useThrottled.js
@@ -0,0 +1,26 @@
+import { useRef, useEffect, useCallback } from "react";
+
+export default (callback, delay, dependencies) => {
+  const throttleRef = useRef(false);
+  const callbackRef = useRef(callback);
+
+  // Update the callback to be used, if it ever changes
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  return useCallback((...args) => {
+    if (throttleRef.current) {
+      console.log("too soon!")
+      return;
+    }
+
+    console.log("OK!")
+    throttleRef.current = true;
+    callbackRef.current(...args);
+    setTimeout(() => {
+      console.log("resetting");
+      throttleRef.current = false;
+    }, delay);
+  }, dependencies);
+};
