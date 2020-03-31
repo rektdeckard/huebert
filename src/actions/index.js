3563768e6cac7ab0204b1da9f093ff25b61c4eb8
Tobias Fried <friedtm@gmail.com>
2020-03-31 00:31:50 -0400

    Refactor actions exports

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,46 +1,7 @@
-export {
-  fetchLights,
-  updateLights,
-  setLight,
-  alertLight,
-  toggleLight,
-  setActiveLight
-} from "./lightActions";
-
-export {
-  fetchGroups,
-  createGroup,
-  deleteGroup,
-  updateGroups,
-  setGroup,
-  alertGroup,
-  toggleGroup,
-  addLight,
-  removeLight,
-  setActiveGroup
-} from "./groupActions";
-
-export {
-  fetchScenes,
-  updateScenes,
-  createScene,
-  deleteScene,
-  modifyScene,
-  setActiveScene
-} from "./sceneActions";
-
-export { fetchSchedules, updateSchedules } from "./scheduleActions";
-
-export { fetchRules, updateRules } from "./ruleActions";
-
-export { fetchSensors, updateSensors, toggleSensor, configureSensor } from "./sensorActions";
-
-export {
-  initializeApp,
-  resetApp,
-  createUser,
-  setTheme,
-  setView,
-  expand,
-  toggleExpanded
-} from "./settingsActions";
+export * from "./lightActions";
+export * from "./groupActions";
+export * from "./sceneActions";
+export * from "./scheduleActions";
+export * from "./ruleActions";
+export * from "./sensorActions";
+export * from "./settingsActions";
